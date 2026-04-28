e516ebdb0bfdb330100167cc6b5f99970541bb7e
ardiansyahraihan389-wq <ardiansyahraihan389@gmail.com>
2026-04-28 13:38:27 +0700

    Delete api_mahasiswa.php

--- api_mahasiswa.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "koneksi.php";
-$sql = "SELECT * FROM mahasiswa";
-$query = mysqli_query($conn, $sql);
-$data = array();
-while ($row = mysqli_fetch_assoc($query)) { $data[] = $row; }
-header('Content-Type: application/json');
-echo json_encode($data);
-?>
