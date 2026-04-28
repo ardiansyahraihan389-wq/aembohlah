d8aab25b50d7c64950b674e72d8738838f34ab63
ardiansyahraihan389-wq <ardiansyahraihan389@gmail.com>
2026-04-28 12:22:22 +0700

    Refactor API response code for mahasiswa data

--- api_mahasiswa.php
+++ api_mahasiswa.php
@@ -1,16 +1,9 @@
 <?php
 include "koneksi.php";
-
 $sql = "SELECT * FROM mahasiswa";
 $query = mysqli_query($conn, $sql);
-
-$data_mahasiswa = array();
-
-while ($row = mysqli_fetch_assoc($query)) {
-    $data_mahasiswa[] = $row;
-}
-
-// Mengatur header agar outputnya berupa JSON
+$data = array();
+while ($row = mysqli_fetch_assoc($query)) { $data[] = $row; }
 header('Content-Type: application/json');
-echo json_encode($data_mahasiswa);
+echo json_encode($data);
 ?>
